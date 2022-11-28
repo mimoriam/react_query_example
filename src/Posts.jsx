d936a91ceb43643a9d5ce6c3c95619d365c4f0e6
Mimo <mimoriam420@gmail.com>
2022-11-28 17:28:23 +0500

    Commit #5 - Pagination support

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,18 +1,24 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useState } from "react";
 
-async function fetchPosts() {
+const maxPostPage = 10;
+
+async function fetchPosts(pageNum) {
     const response = await fetch(
-        `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0`
+        `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
     );
     return response.json();
 }
 
 export const Posts = () => {
+    const [currentPage, setCurrentPage] = useState(1);
 
     const queryClient = useQueryClient();
+
+    // This is a query that supports pagination:
     const { data, isError, error, isLoading } = useQuery(
-        ["posts"],
-        () => fetchPosts(),
+        ["posts", currentPage],
+        () => fetchPosts(currentPage),
         // This is time in milliseconds so 2k = 2 secs
         { staleTime: 2000 }
     );
@@ -27,6 +33,22 @@ export const Posts = () => {
                     {post.title}
                 </li>
             ))}</ul>
+
+            <button
+                disabled={currentPage <= 1}
+                onClick={() => {
+                    setCurrentPage((previousValue) => previousValue - 1)
+                }}>Previous Page
+            </button>
+
+            <span>{currentPage}</span>
+
+            <button
+                disabled={currentPage >= maxPostPage}
+                onClick={() => {
+                    setCurrentPage((previousValue) => previousValue + 1)
+                }}>Next Page
+            </button>
         </>
     )
 };
