fd7b7f5976e80dcf1e670ade44fafc270dce747f
Mimo <mimoriam420@gmail.com>
2022-11-28 16:39:08 +0500

    Commit #2 - Create a QueryClient and provide it to the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Create a client
+const queryClient = new QueryClient();
+
+const App = () => {
+    return (
+        // Provide client to the App:
+        <QueryClientProvider client={queryClient}>
+            <>
+            </>
+        </QueryClientProvider>
+    )
+};
 
 export default App;
