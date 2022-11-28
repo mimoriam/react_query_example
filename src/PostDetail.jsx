e79a0702ff162de8a02f34428171627a661c3a04
Mimo <mimoriam420@gmail.com>
2022-11-28 18:47:12 +0500

    Commit #8 - Update function implemented by ID

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -15,6 +15,14 @@ async function deletePost(postId) {
     return response.json();
 }
 
+async function updatePost(postId) {
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/postId/${postId}`,
+        { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+    );
+    return response.json();
+}
+
 
 export const PostDetail = ({ post }) => {
 
@@ -24,6 +32,7 @@ export const PostDetail = ({ post }) => {
     );
 
     const deleteMutation = useMutation(() => deletePost(post.id));
+    const updateMutation = useMutation(() => updatePost(post.id));
 
     if (isLoading) return <div>Loading...</div>
     if (isError) return <p>{error.toString()}</p>;
@@ -32,8 +41,13 @@ export const PostDetail = ({ post }) => {
         <>
             <h3 style={{ color: "blue" }}>{post.title}</h3>
 
-            <button onClick={() => deleteMutation.mutate()}>Delete</button>
-            <button>Update</button>
+            <button
+                onClick={() => deleteMutation.mutate()}>Delete
+            </button>
+
+            <button
+                onClick={() => updateMutation.mutate()}>Update
+            </button>
 
             {deleteMutation.isError && (
                 <p style={{ color: "red" }}>Error deleting the post</p>
@@ -43,6 +57,16 @@ export const PostDetail = ({ post }) => {
             )}
             {deleteMutation.isSuccess && (
                 <p style={{ color: "green" }}>Post has (not) been deleted</p>
+            )}
+
+            {updateMutation.isError && (
+                <p style={{ color: "red" }}>Error updating the post</p>
+            )}
+            {updateMutation.isLoading && (
+                <p style={{ color: "purple" }}>Updating the post</p>
+            )}
+            {updateMutation.isSuccess && (
+                <p style={{ color: "green" }}>Post has (not) been updated</p>
             )}
 
             <p>{post.body}</p>
