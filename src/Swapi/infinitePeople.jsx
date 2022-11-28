3bf06a97f4bcacdea9cbc76e14e4b09e91d5753c
Mimo <mimoriam420@gmail.com>
2022-11-28 20:48:58 +0500

    Commit #13 - Fixed Infinite loading with isFetchting & initiaLoad={false}

--- src/Swapi/infinitePeople.jsx
+++ src/Swapi/infinitePeople.jsx
@@ -10,29 +10,40 @@ const fetchUrl = async (url) => {
 
 export const InfinitePeople = () => {
 
-    const { data, fetchNextPage, hasNextPage, isLoading, isError, error } = useInfiniteQuery(
+    const { data, fetchNextPage, hasNextPage, isLoading, isError, error, isFetching } = useInfiniteQuery(
         ["sw-people"],
         ({ pageParam = initialUrl }) => fetchUrl(pageParam),
-        // The API has a "next" property so we tell RQ to use that for next page:
+        // The API has a "next" property, so we tell RQ to use that for next page:
         { getNextPageParam: (lastPage) => lastPage.next || undefined }
     );
 
-    if (isLoading) return <div className="loading">Loading...</div>;
+    if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Error! {error.toString()}</div>;
 
-    return <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
-        {data.pages.map(pageData => {
-            return pageData.results.map(person => {
-                return (
-                    <li>
-                        {person.name}
-                        <ul>
-                            <li>hair: {person.hair_color}</li>
-                            <li>eyes: {person.eye_color}</li>
-                        </ul>
-                    </li>
-                )
-            })
-        })}
-    </InfiniteScroll>;
+    return (
+        <>
+            {/*This is crucial to useInfiniteQuery else data jumps back to start*/}
+            {isFetching && <div>Loading...</div>}
+
+            <InfiniteScroll
+                // add initialLoad={false} to prevent loading two pages on component mount:
+                initialLoad={false}
+                loadMore={fetchNextPage}
+                hasMore={hasNextPage}>
+                {data.pages.map(pageData => {
+                    return pageData.results.map(person => {
+                        return (
+                            <li>
+                                {person.name}
+                                <ul>
+                                    <li>hair: {person.hair_color}</li>
+                                    <li>eyes: {person.eye_color}</li>
+                                </ul>
+                            </li>
+                        )
+                    })
+                })}
+            </InfiniteScroll>
+        </>
+    );
 }
